91589398de93be75e794c1147f71621e6320ea16
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-10-03 21:48:22 +0900

    add: アソシエーションの記述

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  has_many :users, through: :members
+  has_many :messages
+  has_many :members
+  accepts_nested_attributes_for :members
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups, through: :members
+  has_many :messages
+  has_many :members
+  accepts_nested_attributes_for :members
 end
