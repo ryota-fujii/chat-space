1df06d35c81a0b47b8da60697b7d8e0771593e42
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-10-04 00:58:48 +0900

    add: アソシエーションの記述

--- db/migrate/20181003124122_create_members.rb
+++ db/migrate/20181003124122_create_members.rb
@@ -1,7 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.1]
   def change
     create_table :members do |t|
-
+      t.integer :user_id,   null: false, foreign_key: true
+      t.integer :group_id,  null: false, foreign_key: true
       t.timestamps
     end
   end
