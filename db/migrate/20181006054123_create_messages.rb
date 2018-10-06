476ca717a5ec89fdedc96430b30b70c299e2c362
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-10-06 15:38:20 +0900

    マイグレーションファイルの記述

--- db/migrate/20181006054123_create_messages.rb
+++ db/migrate/20181006054123_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-
+      t.integer :user_id, null:false
+      t.integer :group_id, null:false
+      t.text :text
+      t.text :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181003124122) do
+ActiveRecord::Schema.define(version: 20181006054123) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20181003124122) do
   create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false
     t.integer "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id", null: false
+    t.integer "group_id", null: false
+    t.text "text"
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
