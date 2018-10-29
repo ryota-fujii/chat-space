1658935f7e4337ab901d97e037300a627975e9f8
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-10-29 15:52:04 +0900

    change: サイドバーに最新の投稿の表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :members
   has_many :users, through: :members
   accepts_nested_attributes_for :members
+
+  def show_last_message
+    if (last_message = messages&.last).present?
+      last_message.content? ? last_message.content : "画像が投稿されています"
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
