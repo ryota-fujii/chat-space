6f54e2b59c2fde779f0c8784e02eede6832f344b
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-11-11 11:11:39 +0900

    fix:エラーの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    end
     if @message.save
       respond_to do |format|
         format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
