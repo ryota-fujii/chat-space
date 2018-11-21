21fba37940089ccef3124a856106af210e283efe
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-11-21 11:36:03 +0900

    change: 三項演算子を記述

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,9 +1,8 @@
 $(function() {
   function buildHTML(message){
-    var insertImage = '';
-    if (message.image){
-      insertImage = `<img src="${message.image}">`;
-    }
+
+    var insertImage =
+      (message.image) ? `<img src="${message.image}">` : '';
       var html =
       `<li class="chat_body">
         <span class="user_name">
