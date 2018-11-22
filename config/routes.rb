97806a95181358274c80e22c8fa44b32c3a0e5ae
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-11-22 11:44:59 +0900

    change: routesにアクションを追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'groups#index'
   resource :user, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
+    collection do
+      get'search'
+    end
     resources :messages, only: [:index, :create]
   end
 end
