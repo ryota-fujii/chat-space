710f0fd70def8ab899d73d71315b69be37f5ba8b
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-11-27 15:09:53 +0900

    change:インクリメンタルサーチの実装

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,37 +4,38 @@ var search_list = $("#user-search-result");
 
 function appendUser(user){
   var html = `
-  <div id="chat-group-users">
-       <input name="chat_group[user_ids][]", type="hidden", value="22"}>
-      <p class="chat-group-user__name">${name}</p>
-    </div>
+  <div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${user.name}</p>
+    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add", data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
   </div>`
   search_list.append(html);
 }
 
-// function appendNoUser() {
-//   var html ='';
-//   search_list.append(html);
-// }
+function appendNoUser(user) {
+  var html ='';
+  search_list.append(html);
+}
 
-  $("#user-search-field.chat-group-form__input").on("keyup", function(e){
-    e.preventDefault();
+  $(".chat-group-form__input").on("keyup", function() {
     var input = $.trim($(this).val());
     console.log(input);
     $.ajax({
       type: 'GET',
-      url: 'user/search',
+      url: '/users/search',
       data: {keyword: input},
       dataType: 'json'
     })
-    console.log("ajax")
 
-    .done(function(data) {
+    .done(function(users) {
+      console.log(users)
       $("#user-search-result").empty();
       if (users.length !== 0){
-        users.foreach(function(user){
+        users.forEach(function(user){
           appendUser(user);
         });
+      }
+      else {
+        appendNoUser("一致するユーザーは存在しません");
       }
     })
     .fail(function(){
