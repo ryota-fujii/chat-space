25f044892bcfd8dbb3c107a5cf883087f52c792f
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-11-27 15:08:21 +0900

    change: searchをusersのアクションにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resource :user, only: [:edit, :update] do
+  resources :users, only: [:edit, :update] do
     collection do
       get'search'
     end
