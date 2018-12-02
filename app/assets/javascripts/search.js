c38be7df253332df30eb64430521e77146c088af
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-12-02 12:28:55 +0900

    fix: エラーの修正、ユーザーの情報を取ってきた

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,7 +6,7 @@ function appendUser(user){
   var html = `
   <div class="chat-group-user clearfix">
     <p class="chat-group-user__name">${user.name}</p>
-    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add", data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
   </div>`
   search_list.append(html);
 }
@@ -16,11 +16,11 @@ function appendNoUser(user) {
   search_list.append(html);
 }
 
-function addUserToGroup(user) {
+function addUserToGroup(id, name) {
   var html = `
   <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-    <input name='group[user_ids][]' type='hidden' value='${user.id}'>
-    <p class='chat-group-user__name'>${user.name}</p>
+    <input name='group[user_ids][]' type='hidden' value='${id}'>
+    <p class='chat-group-user__name'>${name}</p>
     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
   </div>`
   return html;
@@ -32,7 +32,7 @@ function deleteUserFromGroup(user){
 }
   $(".chat-group-form__input").on("keyup", function() {
     var input = $.trim($(this).val());
-    console.log(input);
+    // console.log(input);
     $.ajax({
       type: 'GET',
       url: '/users/search',
@@ -45,6 +45,7 @@ function deleteUserFromGroup(user){
       if (users.length !== 0){
         users.forEach(function(user){
           appendUser(user);
+
         });
       }
       else {
@@ -56,12 +57,15 @@ function deleteUserFromGroup(user){
     });
   });
 
-  $(document).on("click", '.chat-group-user__btn--add', function() {
-    var user = $(this).data();
-    console.log(user);
-    var html = addUserToGroup(user);
+  $("#user-search-result").on("click", ".user-search-add", function() {
+    console.log(this);
+    var user_id = $(this).data('user-id');
+    var user_name = $(this).data('user-name');
+    console.log(user_id);
+    console.log(user_name);
+    var html = addUserToGroup(user_id, user_name);
     console.log(html);
-    $('.chat-group-form__field--right-add').append(html);
-
-  })
+    $('#chat-group-users').append(html);
+    $('')
+  });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def search
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
+      format.html
       format.json
     end
   end
