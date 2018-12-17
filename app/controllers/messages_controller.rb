f925761b5aee9af4f632f1fba8f984152d70ee49
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-12-17 16:37:50 +0900

    fix: indexコントローラーにjsonの記述をした

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,11 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+     binding.pry
+    respond_to do |format|
+      format.html
+      format.json{@new_message = Message.where('id > ?', params[:message][:id])}
+    end
   end
 
   def create
