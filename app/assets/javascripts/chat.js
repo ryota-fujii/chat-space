a3752558c0a6b3465b5bf289288e5250abeaad3c
rits.marianette@gmail.com <rs7328ry@gmail.com>
2018-12-19 17:46:28 +0900

    change: chat.jsの変数message_id -> new_message

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -53,12 +53,12 @@ $(function() {
 
   function update(){
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var message_id = ($('.chat_body')[0]) ? ($('.chat_body:last').data('message-id')):0;
+      var new_message = ($('.chat_body')[0]) ? ($('.chat_body:last').data('message-id')):0;
 
       $.ajax({
         url: location.href,
         type: 'GET',
-        data: {  message: {id: message_id} },
+        data: {  message: {id: new_message} },
         dataType: 'json',
       })
       .done(function(data){
